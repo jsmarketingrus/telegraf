611faa064b56cab1ad42b0ae910a3caf4d7409ab
Tim Zheng <wangztim@berkeley.edu>
2020-10-09 11:42:43 -0400

    Updated comments in assistant.go

--- assistant/assistant.go
+++ assistant/assistant.go
@@ -12,18 +12,20 @@ import (
 	"github.com/influxdata/telegraf/config"
 )
 
-// Assistant represents a client that will connect to the cloud server to allow
-// for plugin management remotely.
+/*
+Assistant is a client to facilitate communications between Agent and Cloud.
+*/
 type Assistant struct {
 	Config     *config.Config              // stores plugins and agent config
-	Requests   chan map[string]interface{} // Queue of requests from server
-	connection *websocket.Conn
-	ctx        context.Context
-	done       chan bool
+	Requests   chan map[string]interface{} // fifo queue of requests from server
+	connection *websocket.Conn             // Active websocket connection
+	ctx        context.Context             // go's context
+	done       chan bool                   // Channel used to stop server listener
 }
 
 // NewAssistant returns an Assistant for the given Config.
 func NewAssistant(ctx context.Context, config *config.Config) (*Assistant, error) {
+	// TODO: Make addr and url dynamic if necessary.
 	var addr = flag.String("addr", "localhost:8080", "http service address")
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
 
@@ -31,6 +33,8 @@ func NewAssistant(ctx context.Context, config *config.Config) (*Assistant, error
 
 	if v, exists := os.LookupEnv("INFLUX_TOKEN"); exists {
 		header.Add("Authorization", "Token "+v)
+	} else {
+		// ? Should we terminate the program? Without a INFLUX_TOKEN we can't authenticate.
 	}
 
 	ws, _, err := websocket.DefaultDialer.Dial(u.String(), header)
@@ -47,22 +51,27 @@ func NewAssistant(ctx context.Context, config *config.Config) (*Assistant, error
 		done:       make(chan bool),
 	}
 
-	go a.serverListener()
+	go a.listenToServer()
 
 	return a, nil
 }
 
+// Stop is ysed to clean up active connection and all channels
 func (assistant *Assistant) Stop() {
 	assistant.connection.Close()
 	close(assistant.Requests)
 	assistant.done <- true
+	close(assistant.done)
 }
 
-/*
-	TODO: Write a dedicated struct for the input of WriteToServer.
-	Refer to the design doc for implementation.
-*/
+// WriteToServer is used as a helper function to write status responses to server.
 func (assistant *Assistant) WriteToServer(payload interface{}) error {
+	/*
+		TODO: Write a dedicated struct for the input of WriteToServer.
+		Refer to the design doc for implementation.
+		https://docs.google.com/document/d/1pnXrWgXCvlpe5tB3YAlOtO6rsvBxoV7DMPHL82KpCok/edit?usp=sharing
+	*/
+
 	err := assistant.connection.WriteJSON(payload)
 	if err != nil {
 		return err
@@ -70,8 +79,8 @@ func (assistant *Assistant) WriteToServer(payload interface{}) error {
 	return nil
 }
 
-// serverListener relays messages from the cloud server to the agent channel.
-func (assistant *Assistant) serverListener() {
+// listenToServer takes requests from the server and puts it in Requests.
+func (assistant *Assistant) listenToServer() {
 	for {
 		var res map[string]interface{}
 		err := assistant.connection.ReadJSON(&res)
@@ -82,10 +91,10 @@ func (assistant *Assistant) serverListener() {
 			assistant.Requests <- res
 		}
 		select {
-		case <-assistant.ctx.Done():
-			return
 		case <-assistant.done:
 			return
+		case <-assistant.ctx.Done():
+			assistant.Stop()
 		default:
 			continue
 		}
