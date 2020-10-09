fca0b54915ed4373bfec86867dadbbef2fca9d67
Tim Zheng <wangztim@berkeley.edu>
2020-10-09 11:27:36 -0400

    Migrate to gorilla ws lib & expose methods

--- assistant/assistant.go
+++ assistant/assistant.go
@@ -2,103 +2,92 @@ package assistant
 
 import (
 	"context"
+	"flag"
 	"fmt"
-	"log"
+	"net/http"
+	"net/url"
+	"os"
 
+	"github.com/gorilla/websocket"
 	"github.com/influxdata/telegraf/config"
-	"golang.org/x/net/websocket"
 )
 
 // Assistant represents a client that will connect to the cloud server to allow
 // for plugin management remotely.
 type Assistant struct {
-	Config *config.Config // stores plugins and agent config
+	Config     *config.Config              // stores plugins and agent config
+	Requests   chan map[string]interface{} // Queue of requests from server
+	connection *websocket.Conn
+	ctx        context.Context
+	done       chan bool
 }
 
 // NewAssistant returns an Assistant for the given Config.
-func NewAssistant(config *config.Config) (*Assistant, error) {
-	a := &Assistant{
-		Config: config,
+func NewAssistant(ctx context.Context, config *config.Config) (*Assistant, error) {
+	var addr = flag.String("addr", "localhost:8080", "http service address")
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
+
+	header := http.Header{}
+
+	if v, exists := os.LookupEnv("INFLUX_TOKEN"); exists {
+		header.Add("Authorization", "Token "+v)
 	}
+
+	ws, _, err := websocket.DefaultDialer.Dial(u.String(), header)
+
+	if err != nil {
+		return nil, err
+	}
+
+	a := &Assistant{
+		Config:     config,
+		Requests:   make(chan map[string]interface{}),
+		connection: ws,
+		ctx:        ctx,
+		done:       make(chan bool),
+	}
+
+	go a.serverListener()
+
 	return a, nil
 }
 
-// client stores the channels to communicate with the agent and the websocket
-// connection to the cloud server.
-// TODO determine if Ctx is necessary, and if so, what for
-type client struct {
-	ServerConn *websocket.Conn
-	FromAgent  chan []byte
-	ToAgent    chan []byte
-	Ctx        context.Context
+func (assistant *Assistant) Stop() {
+	assistant.connection.Close()
+	close(assistant.Requests)
+	assistant.done <- true
 }
 
-// agentListener relays messages received from the agent channel to the cloud
-// server.
-func (cli *client) agentListener() {
-	for {
-		select {
-		case <-cli.Ctx.Done():
-			return
-		case msg := <-cli.FromAgent:
-			fmt.Printf("Assistant received: \"%s\" from agent.\n", msg)
-			if _, err := cli.ServerConn.Write(msg); err != nil {
-				cli.ServerConn.Close()
-				log.Fatal(err)
-			}
-		}
-
+/*
+	TODO: Write a dedicated struct for the input of WriteToServer.
+	Refer to the design doc for implementation.
+*/
+func (assistant *Assistant) WriteToServer(payload interface{}) error {
+	err := assistant.connection.WriteJSON(payload)
+	if err != nil {
+		return err
 	}
+	return nil
 }
 
 // serverListener relays messages from the cloud server to the agent channel.
-func (cli *client) serverListener() {
-	var msg = make([]byte, 512)
-
+func (assistant *Assistant) serverListener() {
 	for {
-		// Block on ServerConn.Read until a message is sent.
-		// Cannot put into a `select` statement because it is not a channel.
-		bytesRead, err := cli.ServerConn.Read(msg)
+		var res map[string]interface{}
+		err := assistant.connection.ReadJSON(&res)
 		if err != nil {
-			log.Fatal(err)
-			break
+			fmt.Println(err)
 		}
-		fmt.Printf("Assistant received: \"%s\" from server.\n", msg[:bytesRead])
+		if len(res) != 0 {
+			assistant.Requests <- res
+		}
 		select {
-		// TODO ensure that this message is handled and actually returns
-		case <-cli.Ctx.Done():
+		case <-assistant.ctx.Done():
 			return
-		case cli.ToAgent <- msg[:bytesRead]:
+		case <-assistant.done:
+			return
+		default:
 			continue
 		}
 	}
-}
-
-// Run is the main function that connects to the cloud server via websocket and
-// starts the Telegraf assistant's listeners.
-func (a *Assistant) Run(ctx context.Context, fromAgent chan []byte, toAgent chan []byte) error {
-	log.Printf("Starting assistant")
-
-	origin := "http://localhost/"
-	url := "ws://localhost:3001/ws"
-	ws, err := websocket.Dial(url, "", origin)
-
-	cli := client{ServerConn: ws, FromAgent: fromAgent, ToAgent: toAgent, Ctx: ctx}
-
-	defer func() {
-		cli.ServerConn.Close()
-		close(cli.ToAgent)
-		close(cli.FromAgent)
-	}()
-
-	if err != nil {
-		// TODO find a mechanism to retry connection or alert the agent
-		log.Fatal(err)
-	}
-
-	go cli.agentListener()
-	go cli.serverListener()
-
-	<-cli.Ctx.Done()
-	return nil
 }
