bb8f40503a649f36d32287c83be29e0f79e40b68
Tim Zheng <wangztim@berkeley.edu>
2020-10-10 20:31:04 -0400

    Linting and added  test code for serverlistener

--- assistant/assistant.go
+++ assistant/assistant.go
@@ -9,21 +9,21 @@ import (
 	"net/url"
 	"os"
 	"time"
-	
+
 	"github.com/gorilla/websocket"
-	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/agent"
+	"github.com/influxdata/telegraf/internal"
 )
 
 /*
 Assistant is a client to facilitate communications between Agent and Cloud.
 */
 type Assistant struct {
-	config     *AssistantConfig     			// stores plugins 
-	connection *websocket.Conn             	// Active websocket connection
-	ctx        context.Context             	// go's context
-	done       chan bool                  	// Channel used to stop server listener
-	agent 	   *agent.Agent					// Pointer to agent to issue commands
+	config     *AssistantConfig // stores plugins
+	connection *websocket.Conn  // Active websocket connection
+	ctx        context.Context  // go's context
+	done       chan bool        // Channel used to stop server listener
+	agent      *agent.Agent     // Pointer to agent to issue commands
 }
 
 /*
@@ -31,12 +31,12 @@ AssistantConfig allows us to configure where to connect to and other params
 for the agent.
 */
 type AssistantConfig struct {
-	Host			string
-	Path			string
-	RetryInterval 	int
+	Host          string
+	Path          string
+	RetryInterval int
 }
 
-func(astConfig *AssistantConfig) fill_defaults() {
+func (astConfig *AssistantConfig) fillDefaults() {
 	if astConfig.Host == "" {
 		astConfig.Host = "localhost:8080"
 	}
@@ -50,7 +50,7 @@ func(astConfig *AssistantConfig) fill_defaults() {
 
 // NewAssistant returns an Assistant for the given Config.
 func NewAssistant(ctx context.Context, config *AssistantConfig, agent *agent.Agent) (*Assistant, error) {
-	config.fill_defaults()
+	config.fillDefaults()
 	var addr = flag.String("addr", config.Host, "http service address")
 	u := url.URL{Scheme: "ws", Host: *addr, Path: config.Path}
 
@@ -59,17 +59,17 @@ func NewAssistant(ctx context.Context, config *AssistantConfig, agent *agent.Age
 	if v, exists := os.LookupEnv("INFLUX_TOKEN"); exists {
 		header.Add("Authorization", "Token "+v)
 	} else {
-		return nil, fmt.Errorf("Influx authorization token not found, please set in env.")
+		return nil, fmt.Errorf("Influx authorization token not found, please set in env")
 	}
 
 	// creates a new websocket connection
 	log.Printf("D! [assistant] Attempting connection to [%s]", config.Host)
 	ws, _, err := websocket.DefaultDialer.Dial(u.String(), header)
-	for err != nil { 		// on error, retry connection again
+	for err != nil { // on error, retry connection again
 		log.Printf("E! [assistant] Failed to connect to [%s], retrying in %ds, "+
 			"error was '%s'", config.Host, config.RetryInterval, err)
 
-		err := internal.SleepContext(ctx, time.Duration(config.RetryInterval) * time.Second)
+		err := internal.SleepContext(ctx, time.Duration(config.RetryInterval)*time.Second)
 		if err != nil {
 			return nil, err
 		}
@@ -83,7 +83,7 @@ func NewAssistant(ctx context.Context, config *AssistantConfig, agent *agent.Age
 		connection: ws,
 		ctx:        ctx,
 		done:       make(chan bool),
-		agent:		agent,
+		agent:      agent,
 	}
 
 	go a.listenToServer()
@@ -99,24 +99,22 @@ func (assistant *Assistant) Stop() {
 }
 
 type plugin struct {
-	Name string
-	Type string
+	Name   string
+	Type   string
 	Config map[string]interface{}
 }
 
 type Request struct {
 	Operation string
-	Uuid string
-	Plugin plugin
+	Uuid      string
+	Plugin    plugin
 }
 
 type Response struct {
 	Status string
-	Uuid string
-	Data interface{}
+	Uuid   string
+	Data   interface{}
 }
-
-
 
 // WriteToServer is used as a helper function to write status responses to server.
 func (assistant *Assistant) WriteToServer(payload Response) error {
@@ -156,13 +154,15 @@ func (assistant *Assistant) listenToServer() {
 		if err != nil {
 			fmt.Println(err)
 		}
-		
+
 		var data string
 		var res Response
 		switch req.Operation {
 		case "GET_PLUGIN":
 			data = "TODO fetch plugin config"
 			res = Response{"SUCCESS", req.Uuid, data}
+			fmt.Print("Received request")
+			fmt.Println(req)
 		case "ADD_PLUGIN":
 			// epic 2
 			res = Response{"SUCCESS", req.Uuid, fmt.Sprintf("%s plugin added.", req.Plugin.Name)}
@@ -181,6 +181,6 @@ func (assistant *Assistant) listenToServer() {
 			res = Response{"ERROR", req.Uuid, "invalid operation request"}
 		}
 		assistant.WriteToServer(res)
-		
+
 	}
 }
