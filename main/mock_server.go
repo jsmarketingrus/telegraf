0858bf2147503e962a49b0ff481f1956e838296a
christykoh <christykoh@berkeley.edu>
2020-12-10 05:17:11 -0800

    mock_server web interface integrated with terminal

--- main/mock_server.go
+++ main/mock_server.go
@@ -5,10 +5,12 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
@@ -16,17 +18,94 @@ import (
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
-var upgrader = websocket.Upgrader{} // use default options
+var upgrader = websocket.Upgrader{}    // use default options
+var webUpgrader = websocket.Upgrader{} // use default options
 
-func echo(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
+var connectAssistantWg sync.WaitGroup // waitgroup to open interface when connection established
+
+var (
+	requestWebChan       = make(chan map[string]interface{})
+	responseWebChan      = make(chan map[string]interface{})
+	requestTerminalChan  = make(chan map[string]interface{})
+	responseTerminalChan = make(chan map[string]interface{})
+)
+
+func webInterface(w http.ResponseWriter, r *http.Request) {
+	// init websocket conn with web server
+	c, err := webUpgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Print("upgrade error: ", err)
+		log.Print("upgrade error:", err)
 		return
 	}
-	reader := bufio.NewReader(os.Stdin)
 	defer c.Close()
+
 	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+
+		var request map[string]interface{}
+		err = json.Unmarshal([]byte(message), &request)
+		if err != nil {
+			log.Println("unmarshal failed with error:", err)
+			break
+		}
+
+		operation, ok := request["operation"]
+		if !ok {
+			log.Println("please specify an operation")
+			continue
+		}
+		plugin, _ := request["name"]
+		pluginType, _ := request["type"]
+		config, _ := request["config"]
+		uniqueId, _ := request["uuid"]
+		fmt.Printf("%s\n", operation)
+		fmt.Printf("Plugin name / unique ID: %v\n", plugin)
+		fmt.Printf("Plugin type: %v\n", plugin)
+		fmt.Printf("Plugin config: %v", config)
+		fmt.Printf("Plugin id: %v", uniqueId)
+
+		uid, _ := uuid.NewRandom()
+
+		// var config map[string]interface{}
+		// _ = json.Unmarshal([]byte(pluginConfig), &config)
+		var m = map[string]interface{}{
+			"Operation": operation,
+			"Uuid":      uid.String(),
+			"Plugin": map[string]interface{}{
+				"Name":     plugin,
+				"Type":     pluginType,
+				"Config":   config,
+				"UniqueId": uniqueId,
+			},
+		}
+
+		// send request
+		requestWebChan <- m
+
+		// write response back to web interface
+		res := <-responseWebChan
+		responseString, err := json.Marshal(res)
+		if err != nil {
+			log.Println("error responding to web interface:", err)
+			break
+		}
+		err = c.WriteMessage(websocket.TextMessage, responseString)
+		if err != nil {
+			log.Println("error writing to web interface:", err)
+			break
+		}
+	}
+}
+
+func terminalInterface() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for { // gather terminal input
 		fmt.Println("\n(0) GET_PLUGIN")
 		fmt.Println("(1) START_PLUGIN")
 		fmt.Println("(2) STOP_PLUGIN")
@@ -44,6 +123,8 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		pluginConfig, _ := reader.ReadString('\n')
 		fmt.Print("Plugin uid: ")
 		uniqueId, _ := reader.ReadString('\n')
+
+		// clean strings
 		plugin = strings.Replace(plugin, "\n", "", -1)
 		pluginType = strings.Replace(pluginType, "\n", "", -1)
 		operation = strings.Replace(operation, "\n", "", -1)
@@ -58,11 +139,7 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		case "2":
 			operation = "STOP_PLUGIN"
 		case "3":
-<<<<<<< HEAD
 			operation = "UPDATE_PLUGIN"
-=======
-			operation = "STOP_PLUGIN"
->>>>>>> 2a7f661aafcaf9d154f60a8b3b55352d685d4d29
 		case "4":
 			operation = "GET_RUNNING_PLUGINS"
 		case "5":
@@ -74,48 +151,252 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		}
 
 		uid, _ := uuid.NewRandom()
+
 		var config map[string]interface{}
 		_ = json.Unmarshal([]byte(pluginConfig), &config)
 		var m = map[string]interface{}{
 			"Operation": operation,
 			"Uuid":      uid.String(),
 			"Plugin": map[string]interface{}{
-				"Name":   plugin,
-				"Type":   pluginType,
-				"Config": config,
+				"Name":     plugin,
+				"Type":     pluginType,
+				"Config":   config,
 				"UniqueId": uniqueId,
 			},
 		}
-		err = c.WriteJSON(m)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
-		_, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
+
+		// write m to channel
+		requestTerminalChan <- m
+
+		// wait for response
+		<-responseTerminalChan
 	}
 }
 
-func convertStringToJSON(s string) ([]byte, error) {
-	req := &Request{}
-	json.Unmarshal([]byte(s), req)
-	return json.Marshal(req)
+func closeAssistantConn(c *websocket.Conn) {
+	c.Close()
 }
 
-type Request struct {
-	Operation string
-	Uuid      string
-	Plugin    map[string]string
+func config(w http.ResponseWriter, r *http.Request) {
+
+	// blocks until connection established
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade error: ", err)
+		return
+	}
+	defer closeAssistantConn(c)
+	// TODO fix thing where negative waitgroup counter
+	connectAssistantWg.Done()
+
+	// when either terminal or
+	for {
+		var m map[string]interface{}
+		select {
+		case m = <-requestWebChan:
+			err = c.WriteJSON(m)
+			if err != nil {
+				log.Println("write:", err)
+				break
+			}
+			_, res, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				break
+			}
+
+			fmt.Print("Web Interface received request: \n %s\n", string(res))
+			log.Println("\nOperation: ")
+
+			var resMap map[string]interface{}
+			_ = json.Unmarshal(res, &resMap)
+
+			responseWebChan <- resMap
+
+		case m = <-requestTerminalChan:
+			err = c.WriteJSON(m)
+			if err != nil {
+				log.Println("write:", err)
+				break
+			}
+			_, res, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				break
+			}
+			log.Printf("recv: %s", res)
+
+			// write m to channel
+			responseTerminalChan <- nil
+		}
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	homeTemplate.Execute(w, "ws://"+r.Host+"/web")
+}
+
+func enableTerminal() {
+	connectAssistantWg.Wait()
+
+	// init terminal interface
+	go terminalInterface()
 }
 
 func main() {
+	connectAssistantWg.Add(1)
+
 	flag.Parse()
 	log.SetFlags(0)
-	http.HandleFunc("/echo", echo)
-	log.Println("listening on localhost:8080/echo...")
+	// web server interface
+	http.HandleFunc("/", home)
+	http.HandleFunc("/web", webInterface)
+	// listens for websocket connection to Assistant
+	http.HandleFunc("/assistant", config)
+	log.Println("web server running on localhost:8080")
+	log.Println("listening on localhost:8080/...\n\n")
+
+	go enableTerminal()
+
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
+
+// adapted from gorilla-websocket's example echo server
+// https://github.com/gorilla/websocket/blob/master/examples/echo
+var homeTemplate = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
+<style>
+      body {
+        font-family: 'Rubik', sans-serif;
+	  }
+	  .outer {
+		height: 95vh;
+		overflow: hidden;
+		display: flex;
+		flex-direction: row;
+	  }
+	  .inner {
+		flex: 1;
+		overflow-y: scroll;
+		width:0;
+		padding: 0% 3%;
+	  }
+    </style>
+<script>  
+window.setInterval(function() {
+	var elem = document.getElementById('output');
+	elem.scrollTop = elem.scrollHeight;
+  }, 5000);
+window.addEventListener("load", function(evt) {
+	var ws;
+	ws = new WebSocket("{{.}}");
+	ws.onmessage = function(evt) {
+		data = JSON.parse(evt.data)
+		printRes("RESPONSE: " + JSON.stringify(data, null, 2));
+	}
+	ws.onerror = function(evt) {
+		data = JSON.parse(evt.data)
+		printRes("RESPONSE: " + JSON.stringify(data, null, 2));
+	}
+
+    var printReq = function(message) {
+		var d = document.createElement("pre");
+		d.style.background = "#3f51b5";
+        d.textContent = message;
+		output.appendChild(d);
+		output.scrollTop = d.offsetTop;
+    };
+    var printRes = function(message) {
+		var d = document.createElement("pre");
+        d.textContent = message;
+        output.appendChild(d);
+    };
+    
+    document.getElementById("requestInput").onclick = function(evt) {
+		evt.preventDefault();
+        if (!ws) {
+            return false;
+		}
+		var operation = document.querySelector("input[name=operation]:checked");
+		var name = document.getElementById("plugin");
+		var uuid = document.getElementById("uuid");
+		var config = document.getElementById("config");
+		req = JSON.stringify({
+			"type": "INPUT",
+			"operation": operation.value,
+			"config": JSON.parse(config.value),
+			"uuid": uuid.value,
+			"name": name.value,
+		}, null, 2)
+		printReq("SEND: " + req);
+        ws.send(req);
+        return false;
+    };
+    document.getElementById("requestOutput").onclick = function(evt) {
+		evt.preventDefault();
+        if (!ws) {
+            return false;
+		}
+		var operation = document.querySelector("input[name=operation]:checked");
+		var name = document.getElementById("plugin");
+		var uuid = document.getElementById("uuid");
+		var config = document.getElementById("config");
+		req = JSON.stringify({
+			"type": "OUTPUT",
+			"operation": operation.value,
+			"config": JSON.parse(config.value),
+			"uuid": uuid.value,
+			"name": name.value,
+		}, null, 2)
+		printReq("SEND: " + req);
+        ws.send(req);
+        return false;
+    };
+});
+</script>
+</head>
+<body>
+<div class="outer">
+<div class="inner">
+<h1>Telegraf Assistant</h1>
+<p>Configure Assistant to connect to "localhost:8080/assistant".
+<p>
+<form>
+<p><input id="get" type="radio" name="operation" value="GET_PLUGIN">
+<label for="get">Get Plugin</label><br>
+<input id="start" type="radio" name="operation" value="START_PLUGIN">
+<label for="start">Start Plugin</label><br>
+<input id="stop" type="radio" name="operation" value="STOP_PLUGIN">
+<label for="stop">Stop Plugin</label><br>
+<input id="update" type="radio" name="operation" value="UPDATE_PLUGIN">
+<label for="update">Update Plugin</label><br>
+<input id="get_running" type="radio" name="operation" value="GET_RUNNING_PLUGINS">
+<label for="get_running">Get Running Plugins</label><br>
+<input id="get_all" type="radio" name="operation" value="GET_ALL_PLUGINS">
+<label for="get_all">Get All Plugins</label><br>
+<input id="get_schema" type="radio" name="operation" value="GET_PLUGIN_SCHEMA">
+<label for="get_schema">Get Plugin Schema</label>
+
+<p><label for="plugin">Plugin Name:</label>
+<input id="plugin" type="text" value="">
+<p><label for="uuid">Plugin Unique ID:</label>
+<input id="uuid" type="text" value="">
+
+<p><label for="config">Plugin Config:</label><br>
+<textarea id="config" name="config" rows="10" cols="35">{}</textarea>
+<p>
+
+<button id="requestInput">Request for Input</button>
+<button id="requestOutput">Request for Output</button>
+</form>
+</div><div class="inner" style="background:#1b1b29;color:white">
+<div id="output" style="overflow-wrap:break-word;"></div>
+</div>
+</body>
+</html>
+`))
